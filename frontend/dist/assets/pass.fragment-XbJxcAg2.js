2abcdfa657ae4785c96d0f1bbce8e427a2289011
Breno Mayder <breno@mayder.com.br>
2026-08-03 01:10:10 -0300

    Atualiza bundle frontend validado
    Check: PATH=/Users/brenomayder/.nvm/versions/node/v22.22.0/bin:$PATH RUN_PYTHON_TESTS=1 RUN_FRONTEND_CHECKS=1 ./check.sh

---
+++ frontend/dist/assets/pass.fragment-XbJxcAg2.js
@@ -0,0 +1,4 @@
+import{S as r}from"./sindarius-gcodeviewer.es-BKexLCbK.js";import"./index-BO10u7NU.js";import"./react-vendor-9t-2b6dQ.js";import"./icons-CNFEcl-l.js";const e="passPixelShader",t=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {fragmentOutputs.color=textureSample(textureSampler,textureSamplerSampler,input.vUV);}`;r.ShadersStoreWGSL[e]||(r.ShadersStoreWGSL[e]=t);const n={name:e,shader:t};export{n as passPixelShaderWGSL};
---
+++ frontend/dist/assets/passCube.fragment-BaofyAMl.js
@@ -0,0 +1,23 @@
+import{S as r}from"./sindarius-gcodeviewer.es-BKexLCbK.js";import"./index-BO10u7NU.js";import"./react-vendor-9t-2b6dQ.js";import"./icons-CNFEcl-l.js";const e="passCubePixelShader",t=`varying vec2 vUV;uniform samplerCube textureSampler;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{vec2 uv=vUV*2.0-1.0;
+#ifdef POSITIVEX
+gl_FragColor=textureCube(textureSampler,vec3(1.001,uv.y,uv.x));
+#endif
+#ifdef NEGATIVEX
+gl_FragColor=textureCube(textureSampler,vec3(-1.001,uv.y,uv.x));
+#endif
+#ifdef POSITIVEY
+gl_FragColor=textureCube(textureSampler,vec3(uv.y,1.001,uv.x));
+#endif
+#ifdef NEGATIVEY
+gl_FragColor=textureCube(textureSampler,vec3(uv.y,-1.001,uv.x));
+#endif
+#ifdef POSITIVEZ
+gl_FragColor=textureCube(textureSampler,vec3(uv,1.001));
+#endif
+#ifdef NEGATIVEZ
+gl_FragColor=textureCube(textureSampler,vec3(uv,-1.001));
+#endif
+}`;r.ShadersStore[e]||(r.ShadersStore[e]=t);const v={name:e,shader:t};export{v as passCubePixelShader};
